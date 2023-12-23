5c65efcd6b40f7c1614faa54b8a5b14d91bd0a4c
cedric <andzamac12@gmail.com>
2023-12-24 00:32:54 +0100

    commit

--- src/vendeur.c
+++ src/vendeur.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <outil.h>
-#include <produit.h>
+#include <vendeur.h>
 #include<string.h>
 
 void saisirVendeur(FILE* F,vendeur* v)
@@ -27,4 +27,37 @@ void saveVendeurs(FILE* F, vendeur* TAB,int nombre_vendeur)
     int i;
     for(i=0 ; i<nombre_vendeur ; i++)
         saisirVendeur(F, &TAB[i]);
+}
+void ajouterVendeur(void)
+{
+    vendeur v;
+    FILE *F=fopen(FILE_VENDEUR, "w");
+    if(F==NULL)
+    {
+        printf("Erreur d'ouverture %s", FILE_VENDEUR);
+        exit(-1);
+    }
+    printf("Entrez le matricule du vendeur : ");
+    scanf("%s", v.matricule_vendeur);
+    printf("Entrez le nom du vendeur : ");
+    scanf("%s", v.nom_vendeur);
+    printf("Entrez le salaire du vendeur : ");
+    scanf("%d", &v.salaire);
+    saisirVendeur(F, &v);
+    fclose(F);
+}
+void modifierVendeur(void)
+{
+    vendeur TAB[SIZE];
+    char matricule[MAX];
+    bool trouver;
+    int nombre_vendeur;
+
+    FILE* F=fopen(FILE_VENDEUR, "r");
+    if(F==NULL)
+    {
+        printf("Erreur d'ouverture %s ", FILE_VENDEUR);
+        exit(-1);
+    }
+    
 }
