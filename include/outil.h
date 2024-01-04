f7d349a16e7b458647197f68530c550d3fd68ef3
cedric <andzamac12@gmail.com>
2024-01-04 08:53:29 +0100

    debut de l'elaboration de la facture

--- include/outil.h
+++ include/outil.h
@@ -1,13 +1,14 @@
 #ifndef __OUTIL__H__
 #define __OUTIL__H__
-
+#define SIZE 5
 
 // fonction et outil necessaire pour le projet
 typedef enum bool
 {
     false,true
 }bool;
-
+void etablirFActure(const char*);
+void affichage(void);
 #define MAX 15
 #define SIZE 1000
 
